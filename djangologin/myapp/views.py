293972ab42d7634125f1da5d761a8957be59be5f
Eugeneas3 <eugenesoftengr@gmail.com>
2024-05-28 10:55:58 +0300

    my second commit

--- djangologin/myapp/views.py
+++ djangologin/myapp/views.py
@@ -2,5 +2,9 @@ from urllib import request
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def index(requests):
+    return render(requests, 'index.html')
+
+def djangologin(requests):
+    pass
+    
